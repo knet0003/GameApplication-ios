02e530ef4d2c29b38f49402f4407bec9667c061b
Keovechchta Net <knet0003@student.monash.edu>
2020-10-23 18:22:45 +1100

    Initial Commit

---
+++ Assessment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assessment2
+//
+//  Created by Net Keovechchta on 2020/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
